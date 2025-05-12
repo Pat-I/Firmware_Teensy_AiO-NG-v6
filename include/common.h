2bd86f73d95384d13e832cd4d174dccf36cedaa2
mtz8302 <35091324+mtz8302@users.noreply.github.com>
2025-05-12 05:32:55 +0200

    steering soft start
    ramp up PWM from 0 at start.
    time adjustable
    debug modes:
    a = ADC debug for WAS
    p = PWM debug

--- include/common.h
+++ include/common.h
@@ -114,8 +114,9 @@ uint8_t aog2Count = 0;
 uint8_t xte = 0;
 
 // pwm variables
-int16_t pwmDrive = 0, pwmDisplay = 0;
+int16_t pwmDrive = 0, pwmDisplay = 0, SteerPWMonStartRampTime = 1000;
 float highLowPerDeg = 0;
+bool pwmDebug = false;
 
 // Variables for settings
 struct SteerSettingsStruct
@@ -137,6 +138,7 @@ int16_t lastEnc = -999;
 bool autoSteerEnabled = false;
 float steerAngleActual = 0;
 int16_t steeringPosition = 0; // from steering sensor (WAS)
+bool adcDebug = false;
 // End
 
 // Switches/Sensors
--- include/debug.h
+++ include/debug.h
@@ -26,7 +26,13 @@ void checkUSBSerial()
       Serial.print("\r\nSetting CPU usage debug: ");
       Serial.print(printCpuUsages);
     }
-
+    
+    else if (usbRead == 'a') // output WAS adc data
+    {
+      adcDebug = !adcDebug;
+      Serial.print("\r\nSetting WAS ADC value debug: ");
+      Serial.print(adcDebug);
+    }
 
     else if (usbRead == 'g' && Serial.available() > 0) // temporarily set GPS fix state according to standard GGA fix numbers (see LEDS.h, setGpsLED())
     {
@@ -85,6 +91,12 @@ void checkUSBSerial()
       }
     }
 
+    else if (usbRead == 'p') // output PWM and current sensor data
+    {
+      pwmDebug = !pwmDebug;
+      Serial.print("\r\nSetting PWM / current sensor debug: ");
+      Serial.print(pwmDebug);
+    }
 
     else if (usbRead == 'r')
     {
